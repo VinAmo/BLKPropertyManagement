9b2dc8a1e3352525fbef21e31e868204e00ddda3
VinAmo <philoamo@yahoo.com>
2015-06-04 09:42:48 +0800

    Initial Commit

---
+++ BLKPropertyManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLKPropertyManagement
+//
+//  Created by blk01 on 15/6/4.
+//  Copyright (c) 2015年 BLK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLKPropertyManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLKPropertyManagement
+//
+//  Created by blk01 on 15/6/4.
+//  Copyright (c) 2015年 BLK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
